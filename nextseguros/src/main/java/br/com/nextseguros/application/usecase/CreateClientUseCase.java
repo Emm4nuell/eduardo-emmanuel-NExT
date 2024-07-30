761574e0297023f25f7e5df3e32b671c3c2cb187
Emm4nuell <eduardo.eesf@gmail.com>
2024-07-30 14:41:47 -0300

    Created interface and class CreateClientUseCase

---
+++ nextseguros/src/main/java/br/com/nextseguros/application/usecase/CreateClientUseCase.java
@@ -0,0 +1,10 @@
+package br.com.nextseguros.application.usecase;
+
+import br.com.nextseguros.application.domain.model.ClientModel;
+import br.com.nextseguros.application.port.in.ICreateClientUseCase;
+
+public class CreateClientUseCase implements ICreateClientUseCase {
+    @Override
+    public void execute(ClientModel clientModel) {
+    }
+}
