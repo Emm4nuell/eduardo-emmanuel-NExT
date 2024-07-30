1433db39706be8f38cad5cc8e28db4b7ed96b8e9
Emm4nuell <eduardo.eesf@gmail.com>
2024-07-30 13:13:10 -0300

    first commit

---
+++ nextseguros/src/main/java/br/com/nextseguros/NextSegurosApplication.java
@@ -0,0 +1,11 @@
+package br.com.nextseguros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextSegurosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NextSegurosApplication.class, args);
+    }
+}
