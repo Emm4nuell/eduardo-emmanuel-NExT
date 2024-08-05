f6ca5e8d8a5f071126bdc077035008f4ff20d0e6
Emm4nuell <eduardo.eesf@gmail.com>
2024-08-05 14:50:45 -0300

    Created module HouseService

---
+++ HouseService/src/main/java/br/com/houseservice/HouseServiceApplication.java
@@ -0,0 +1,11 @@
+package br.com.houseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HouseServiceApplication.class, args);
+    }
+}
